5db25b4fd26e298e9bef244dc98a002676657997
Paul Ashour <paul.ashour@gmail.com>
2018-10-02 16:46:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-  array.join(', ')
+  if array.length < 3
+    array.join(' and ')
+  else
+    str_list = array[0..array.length - 2].join(', ')
+    str_list << " and #{array[-1]}"
 end
